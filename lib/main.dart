c854a15c122ed78206e6e4540b2d2a1e9ec0bb37
Paul Slinkard <slinkardpaul50@gmail.com>
2025-10-28 16:19:20 -0500

    refactor: Extract MyPicsState into pictureFrame.dart
    This commit refactors the `MyPicsState` class by extracting it from `main.dart` into a new, dedicated file, `lib/pictureFrame.dart`.
    
    This improves code organization and modularity by separating the stateful widget logic from the main application entry point.
    
    Specific changes:
    - Created `lib/pictureFrame.dart` to house the `MyPicsState` class.
    - Moved the entire `_MyPicsState` implementation, now named `MyPicsState`, to the new file.
    - Updated `main.dart` to import `pictureFrame.dart` and reference the external `MyPicsState` in the `createState` method of the `MyPics` widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_ablum_slider/pictureFrame.dart';
 
 void main() {
   runApp(const MyPics());
@@ -8,79 +9,5 @@ class MyPics extends StatefulWidget {
   const MyPics({super.key});
 
   @override
-  State<MyPics> createState() => _MyPicsState();
-}
-
-class _MyPicsState extends State<MyPics> {
-  // Creating a PageController to control the scrolling of the photos
-  late final PageController _pageController;
-  int _currentPage = 0; // To keep track of the current page
-
-  // Some placeholders for the photos
-  final List<Color> _photoPlaceholders = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.orange,
-    Colors.purple,
-    Colors.yellow,
-    Colors.pink,
-    Colors.brown,
-    Colors.grey,
-    Colors.indigo,
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    // Initializing the PageController
-    _pageController = PageController(initialPage: _currentPage);
-
-    // Adding a listener to the PageController to update the current page
-    _pageController.addListener(() {
-      setState(() {
-        _currentPage = _pageController.page!.round();
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    // Dispose the PageController to avoid memory leaks
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Photo Album Slider'),
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
-        ),
-        body: Center(
-          // Controlling the size of the photo slider
-          child: SizedBox(
-            height: 300, // Height of the slider area
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: _photoPlaceholders.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: _photoPlaceholders[index],
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  State<MyPics> createState() => MyPicsState();
 }
